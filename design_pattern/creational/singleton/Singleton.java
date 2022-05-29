d93fa08c4251d667251c28c24c8b2ae1b991dd9c
whdlsrnr0731 <whdlsrnr0731@gmail.com>
2022-05-29 19:26:47 +0900

    싱글톤 패턴 기본

--- design_pattern/creational/singleton/Singleton.java
+++ design_pattern/creational/singleton/Singleton.java
@@ -2,11 +2,14 @@ package design_pattern.creational.singleton;
 
 public class Singleton {
 
-    private static final Singleton INSTANCE = new Singleton();
+    private static Singleton INSTANCE = null;
 
     private Singleton() {}
 
     public static Singleton getInstance() {
+        if(INSTANCE == null){
+            INSTANCE = new Singleton();
+        }
         return INSTANCE;
     }
 }
