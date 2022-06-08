206371f44f2830443632dd29eb95d96298125989
whdlsrnr0731 <whdlsrnr0731@gmail.com>
2022-06-08 09:38:10 +0900

    빌더 패턴

--- design_pattern/creational/builder/BuilderPattern.java
+++ design_pattern/creational/builder/BuilderPattern.java
@@ -2,7 +2,10 @@ package design_pattern.creational.builder;
 
 public class BuilderPattern {
     public static void main(String []args) {
-        Rect rect = new Rect.Builder(200, 100).margin(5).padding(10).build();
+        Rect rect = new Rect.Builder(200, 100)
+                            .margin(5)
+                            .padding(10)
+                            .build();
         System.out.println(rect.toString());
     }
 }
