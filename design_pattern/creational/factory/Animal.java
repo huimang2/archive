2076627e12eddd1eed05b9cf625172cc37f06fb1
whdlsrnr0731 <whdlsrnr0731@gmail.com>
2022-06-18 00:56:30 +0900

    코드 연습

--- design_pattern/creational/factory/Animal.java
+++ design_pattern/creational/factory/Animal.java
@@ -5,7 +5,6 @@ public abstract class Animal {
     public abstract String getName();
     public abstract String getType();
 
-    @Override
     public String toString() {
         return "Animal Type : " + getType() + "\n" + "Animal Name : " + getName() + "\n";
     }
