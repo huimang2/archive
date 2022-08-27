96c7267924201a8dd50bc86e11394443b1319253
Rubisco <whdlsrnr0731@gmail.com>
2022-08-27 22:47:32 +0900

    update

--- programmers/javascript/Lv2/12973_짝지어 제거하기.js
+++ programmers/javascript/Lv2/12973_짝지어 제거하기.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 // https://programmers.co.kr/learn/courses/30/lessons/12973
 
 function solution(s) {
     return s.replace(/\w{2}/g, '') ? 0 : 1;
+=======
+function solution(s)
+{
+    s.replace('/w{2}')
+
+    return answer;
+>>>>>>> d3e38e6 (update)
 }
