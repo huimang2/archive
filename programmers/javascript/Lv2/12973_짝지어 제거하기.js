3743c6b90d796d2ca70ce3f31388105ae888e70d
Rubisco <whdlsrnr0731@gmail.com>
2022-10-17 01:29:32 +0900

    프로그래머스 12973 문제 풀이

--- programmers/javascript/Lv2/12973_짝지어 제거하기.js
+++ programmers/javascript/Lv2/12973_짝지어 제거하기.js
@@ -1,6 +1,5 @@
-function solution(s)
-{
-    s.replace('/w{2}')
+// https://programmers.co.kr/learn/courses/30/lessons/12973
 
-    return answer;
+function solution(s) {
+    return s.replace(/\w{2}/g, '') ? 0 : 1;
 }
