b42cb1c38972562aa8d484826e7621b46b11a805
Rubisco <whdlsrnr0731@gmail.com>
2022-10-17 01:29:32 +0900

    프로그래머스 118666 문제 풀이

---
+++ programmers/javascript/Lv2/131704_택배상자.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/118666
+
+function solution(order) {
+    
+    const container = Array.from({length: order.length}, (_,i) => i+1);
+    let subContainer = [], result = 0;
+
+    for(let i = 0; i < order.length; i++) {
+        container.
+        while(order.length && order[i])
+        if(subContainer.pop() === order[i]) result++;
+        else return result;
+    }
+
+    return result;
+}
