09c5b9dae52e672e91b60077747cb1ebf598872a
Rubisco <whdlsrnr0731@gmail.com>
2022-10-18 13:23:26 +0900

    프로그래머스 12973 문제 풀이

--- programmers/javascript/Lv2/12973_짝지어 제거하기.js
+++ programmers/javascript/Lv2/12973_짝지어 제거하기.js
@@ -1,5 +1,8 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12973
 
 function solution(s) {
-    return s.replace(/\w{2}/g, '') ? 0 : 1;
+    const arr = [];
+    for(let i = 0, length = s.length; i < length; i++)
+        !arr.length || arr[arr.length-1] !== s[i] ? arr.push(s[i]) : arr.pop();
+    return arr.length ? 0 : 1;
 }
